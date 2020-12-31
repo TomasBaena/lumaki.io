825d2db198f86c776dcf4e6dcfed59fef230c966
Tomas Baena <tomas@lumaki.io>
2020-12-31 16:04:44 -0500

    add relative url so local and prod work

--- src/App.js
+++ src/App.js
@@ -6,9 +6,9 @@ import  Breadhound from './components/breadhound.js';
 function App() {
   return (
     <div className="App">
-      <Route exact path="/"><Home /></Route>
-      <Route path="/about"><About /></Route>     
-      <Route path="/breadhound"><Breadhound /></Route>  
+      <Route exact path={ process.env.PUBLIC_URL + "/"}><Home /></Route>
+      <Route path={ process.env.PUBLIC_URL + "/about"}><About /></Route>     
+      <Route path={ process.env.PUBLIC_URL + "/breadhound"}><Breadhound /></Route>  
     </div>
   );
 }
