d8fa47ab2fd5c478d17ff3d724eac0664cdf0c9a
Tomas Baena <tomas@lumaki.io>
2020-12-31 14:18:54 -0500

    homepage wip

--- src/App.js
+++ src/App.js
@@ -1,23 +1,68 @@
-import logo from './logo.svg';
+import logoLight from './assets/logo/lumaki-light.svg';
+import rocket from './assets/rocket.svg';
+import exhaust from './assets/exhaust.svg';
+import breadhoundLogo from './assets/breadhound/breadhound-right.svg';
 import './App.css';
+import { Button } from 'antd';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App">  
+      <div className="Body-container">
+        <div className="Left-container">
+          <div className="Inner-container">
+            <img src={logoLight} className="Lumaki-logo" alt="logo" />
+            <h2 className="Header-text">Taking ideas to new places</h2>
+            <p className="Sub-text">2021 brings the release of our newest creation. Take control of your
+              spending habits by tracking where your money goes. breadhound helps you
+              categorize your transactions and displays how much money has been spent
+              and saved so you can improve month over month. 
+            </p>
+            <div className="Button-group">
+              <Button 
+                type="link" 
+                size="large" 
+                href="/about"
+                style={{ color: "aliceblue" }}>About us</Button>
+              <Button 
+                type="primary" 
+                size="large" 
+                href="/breadhound"
+                style={{ background: "#42d77d", border: "none", marginLeft: "10px" }}>
+                  Check out breadhound
+                  <img 
+                     className="Breadhound-icon"
+                    src={breadhoundLogo} 
+                    alt="breadhound"/>
+              </Button>
+            </div>
+          </div>
+        </div>
+        <div className="Right-container">
+          <div className="Rocket-container">
+            <img src={exhaust} className="Exhaust" alt="exhaust" />
+            <img src={rocket} className="Rocket" alt="rocket" />      
+          </div>
+        </div>
+      </div>
+      <div className="Star-container">
+        <div className="star star-1"></div>
+        <div className="star star-2"></div>
+        <div className="star star-3"></div>
+        <div className="star star-4"></div>
+        <div className="star star-5"></div>
+        <div className="star star-6"></div>
+        <div className="star star-7"></div>
+        <div className="star star-8"></div>
+        <div className="star star-9"></div>
+        <div className="star star-10"></div>
+        <div className="star star-11"></div>
+        <div className="star star-12"></div>
+        <div className="star star-13"></div>
+        <div className="star star-14"></div>
+        <div className="star star-15"></div>
+        <div className="star star-16"></div> 
+      </div>
     </div>
   );
 }
